0b3ab906bcb858e456519ed2423a2b1a844c5ef9
Matias Amuchástegui <matias.amuchastegui@mercadolibre.com>
2021-09-04 15:54:30 -0300

    setting ip to 3000

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-//const { networkInterfaces } = require('os');
 
 const app = express();
 
@@ -48,7 +47,7 @@ console.log("IP ", adresses)
 
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
