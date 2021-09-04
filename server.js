2b20fc5000be6775a6273f336ec631743925ec95
Matias Amuchástegui <matias.amuchastegui@mercadolibre.com>
2021-09-04 15:52:14 -0300

    log ip

--- server.js
+++ server.js
@@ -35,21 +35,16 @@ db.mongoose
         process.exit();
     });*/
 
-/*const nets = networkInterfaces();
-const results = Object.create(null); // Or just '{}', an empty object
+// Network interfaces
+var ifaces = require('os').networkInterfaces();
 
-for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-        if (net.family === 'IPv4' && !net.internal) {
-            if (!results[name]) {
-                results[name] = [];
-            }
-            results[name].push(net.address);
-            console.log('Ips', results);
-        }
-    }
-}*/
+// Iterate over interfaces ...
+var adresses = Object.keys(ifaces).reduce(function (result, dev) {
+    return result.concat(ifaces[dev].reduce(function (result, details) {
+        return result.concat(details.family === 'IPv4' && !details.internal ? [details.address] : []);
+    }, []));
+});
+console.log("IP ", adresses)
 
 
 // set port, listen for requests
